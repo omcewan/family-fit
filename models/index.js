87fe72b7e7cee0993dd1af5f50dfdde91e95ff3a
omcewan <orlandomcewan231@gmail.com>
2022-03-08 20:28:55 -0500

    create Workout model and associations with Workout

--- models/index.js
+++ models/index.js
@@ -1,13 +1,22 @@
 // inmport modules
 const Family = require('./family');
 const Member = require('./member');
+const Workout = require('./workout');
 
 Family.hasMany(Member, {
-  foreignKey: 'family_id'
-})
+  foreignKey: 'family_id',
+});
 
 Member.belongsTo(Family, {
-  foreignKey: 'family_id'
-})
+  foreignKey: 'family_id',
+});
 
-module.exports = { Family, Member };
+Member.hasMany(Workout, {
+  foreignKey: 'member_id',
+});
+
+Workout.belongsToMany(Member, {
+  foreignKey: 'member_id',
+});
+
+module.exports = { Family, Member, Workout };
