2af84917749ef7c6115489406b33d1bbaf7a43d7
omcewan <orlandomcewan231@gmail.com>
2022-03-10 19:56:58 -0500

    create route to obtain single family and its members

--- routes/apiRoutes/family-routes.js
+++ routes/apiRoutes/family-routes.js
@@ -1,12 +1,27 @@
 const router = require('express').Router();
-const { Family } = require('../../models');
+const { Family, Member } = require('../../models');
 
-// get all family
+// get all families and their memebers
 router.get('/', (req, res) => {
-  Family.findAll()
+  Family.findAll({
+    include: { model: Member },
+  })
     .then((familyData) => {
       res.json(familyData);
     })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// get a single family and its members
+router.get('/:id', (req, res) => {
+  Family.findOne({
+    include: { model: Member },
+    where: { id: req.params.id },
+  })
+    .then((familyData) => res.json(familyData))
     .catch((err) => {
       console.log(err);
       res.status(500).json(err);
