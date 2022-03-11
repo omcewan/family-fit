9fe590cea08b9aa913053194f57635b123b5469c
omcewan <orlandomcewan231@gmail.com>
2022-03-10 20:53:07 -0500

    create put route to update family

--- routes/apiRoutes/family-routes.js
+++ routes/apiRoutes/family-routes.js
@@ -34,9 +34,34 @@ router.get('/:id', (req, res) => {
     });
 });
 
+// create a new family
 router.post('/', (req, res) => {
   Family.create({ family_name: req.body.family_name })
     .then((familyData) => {
+      res.json(familyData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// update a family
+router.put('/:id', (req, res) => {
+  Family.update(
+    { family_name: req.body.family_name },
+    { where: { id: req.params.id } }
+  )
+    .then((familyData) => {
+      if (!familyData[0]) {
+        res
+          .status(400)
+          .json({
+            message:
+              'No Family with with that ID exists / Update already applied!',
+          });
+        return;
+      }
       res.json(familyData);
     })
     .catch((err) => {
