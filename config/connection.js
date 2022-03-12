75d7f649cbf6eb85fa6a9ceebc41e1b89d18969c
omcewan <orlandomcewan231@gmail.com>
2022-03-12 15:01:11 -0500

    pull walt's branch and test code

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,12 @@
 require('dotenv').config();
 const Sequelize = require('sequelize');
-console.log(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW)
+
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: 'localhost' ,
-      port: 3306,
+    // if connection doesn't work try changing host to 'localhost'
+      host: '127.0.0.1' ,
+      // port: 3306,
       dialect: 'mysql',
     });
 
