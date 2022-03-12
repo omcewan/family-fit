60e4f6a4f72a4de36627b42bebb2d4cf11646139
omcewan <orlandomcewan231@gmail.com>
2022-03-12 14:37:13 -0500

    create post and delete routes for workout out model

--- routes/apiRoutes/workout-routes.js
+++ routes/apiRoutes/workout-routes.js
@@ -13,4 +13,33 @@ router.get('/', (req, res) => {
     });
 });
 
+// create a new workout
+router.post('/', (req, res) => {
+  Workout.create({
+    workout_name: req.body.workout_name,
+  })
+    .then((workoutData) => {
+      res.json(workoutData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.delete('/:id', (req, res) => {
+  Workout.destroy({ where: { id: req.params.id } })
+    .then((workoutData) => {
+      if (!workoutData) {
+        res.status(400).json({ message: 'No Workout with that ID exists!' });
+        return;
+      }
+      res.json(workoutData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
 module.exports = router;
