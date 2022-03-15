b8633a546698fe69117055beb2d536ed608d08cb
omcewan <orlandomcewan231@gmail.com>
2022-03-14 20:43:34 -0400

    set up chart js data to be dynamic to the user

--- public/javascript/chart.js
+++ public/javascript/chart.js
@@ -1,38 +1,45 @@
 const ctx = document.getElementById('myChart');
-const labels = [
-  'Monday',
-  'Tuesday',
-  'Wednesday',
-  'Thursday',
-  'Friday',
-  'Saturday',
-  'Sunday',
-];
-const data = {
-  labels: labels,
-  datasets: [
-    {
-      label: 'Test Graph',
-      backgroundColor: 'rgb(255, 99, 132)',
-      borderColor: 'rgb(255, 99, 132)',
-      data: [5, 10, 15, 20, 25, 30, 35],
-    },
-  ],
-};
-
-const config = {
-  type: 'line',
-  data: data,
-  options: {},
-};
-
-// const myChart = new Chart(ctx, config);
 
 async function chartData() {
-  const data = fetch('/api/members');
-  if(data.ok) {
-    console.log(data)
+  const response = await fetch('/api/members/1');
+  if (response.ok) {
+    const memberData = response.json();
+    return memberData;
   }
 }
 
-chartData();
+let workoutHours;
+
+chartData().then((resData) => {
+  let workouts = resData['logged_workouts'];
+  workoutHours = workouts.map((element) => element.hours);
+  console.log(workoutHours);
+  const labels = [
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+    'Sunday',
+  ];
+  const data = {
+    labels: labels,
+    datasets: [
+      {
+        label: 'Test Graph',
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgb(255, 99, 132)',
+        data: workoutHours,
+      },
+    ],
+  };
+
+  const config = {
+    type: 'line',
+    data: data,
+    options: {},
+  };
+
+  const myChart = new Chart(ctx, config);
+});
