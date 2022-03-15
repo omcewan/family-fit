fa9073596186b7302cbaacf8d6f4be35544b22bd
omcewan <orlandomcewan231@gmail.com>
2022-03-15 17:44:07 -0400

    refactor chart.js and implement labels to be the createdAt column from database

--- public/javascript/chart.js
+++ public/javascript/chart.js
@@ -1,45 +1,36 @@
 const ctx = document.getElementById('myChart');
 
 async function chartData() {
-  const response = await fetch('/api/members/1');
+  const response = await fetch('/api/members/2');
   if (response.ok) {
-    const memberData = response.json();
-    return memberData;
+    const memberData = await response.json();
+    const workout = await memberData['logged_workouts'];
+    console.log(workout);
+    const workoutHours = await workout.map((element) => element.minutes);
+    const labels = await workout.map((element) =>
+      element.createdAt.substring(0, 10)
+    );
+
+    const data = {
+      labels: labels,
+      datasets: [
+        {
+          label: 'Test Graph',
+          backgroundColor: 'rgb(255, 99, 132)',
+          borderColor: 'rgb(255, 99, 132)',
+          data: workoutHours,
+        },
+      ],
+    };
+
+    const config = {
+      type: 'line',
+      data: data,
+      options: {},
+    };
+
+    const myChart = new Chart(ctx, config);
   }
 }
 
-let workoutHours;
-
-chartData().then((resData) => {
-  let workouts = resData['logged_workouts'];
-  workoutHours = workouts.map((element) => element.minutes);
-  console.log(workoutHours);
-  const labels = [
-    'Monday',
-    'Tuesday',
-    'Wednesday',
-    'Thursday',
-    'Friday',
-    'Saturday',
-    'Sunday',
-  ];
-  const data = {
-    labels: labels,
-    datasets: [
-      {
-        label: 'Test Graph',
-        backgroundColor: 'rgb(255, 99, 132)',
-        borderColor: 'rgb(255, 99, 132)',
-        data: workoutHours,
-      },
-    ],
-  };
-
-  const config = {
-    type: 'line',
-    data: data,
-    options: {},
-  };
-
-  const myChart = new Chart(ctx, config);
-});
+chartData();
